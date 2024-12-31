236a8e40bdb4843c8d0006a4f1ccd26cd20ed8d6
kingabdulwasay <awasay245@gmail.com>
2024-12-31 08:47:31 -0800

    Remove Play Method

--- src/main/java/com/hawm/snakeandladder/classes/Game.java
+++ src/main/java/com/hawm/snakeandladder/classes/Game.java
@@ -6,48 +6,11 @@ public class Game {
     private List<Player> players;
     private Board board;
     private static Random dice = new Random();
-    private Scanner scanner;
 
 
-
-    public void play() {
-        boolean gameWon = false;
-        while (!gameWon) {
-            for (Player player : players) {
-                System.out.println(player.getName() + ", press Enter to roll the dice.");
-                scanner.nextLine();
-
-                int diceRoll = dice.nextInt();
-                System.out.println(player.getName() + " rolled a " + diceRoll);
-
-                int newPosition = player.getPosition() + diceRoll;
-                if (newPosition >= 99) {
-                    System.out.println(player.getName() + "  IS THE WINNER!!!!");
-                    gameWon = true;
-                    break;
-                }
-
-                int moveEffect = board.getMoveEffect(newPosition);
-                if (moveEffect != 0) {
-                    if (moveEffect > 0) {
-                        System.out.println(player.getName() + " found a ladder and climbed to " + (newPosition + moveEffect));
-                    } else {
-                        System.out.println(player.getName() + " WAS BIT BY A SNAKE AND WENT BACK to " + (newPosition + moveEffect));
-
-                    }
-                    newPosition += moveEffect;
-                }
-
-                player.setPosition(newPosition);
-                System.out.println(player.getName() + " is now at position " + newPosition);
-            }
-        }
-    }
     public static int rollDice() {
         return dice.nextInt(6) + 1;
     }
-    public static void snake(Player player, int position){
 
-    }
 
 }
